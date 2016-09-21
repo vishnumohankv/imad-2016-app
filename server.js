c0eb4c21be4858c5ab01b21a74481dd296e56a4f
vishnumohankv <vishnukizhakkumkara@gmail.com>
2016-09-21 21:58:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/article1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
-app.get('/article2', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
-  app.get('ui/article3', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
-});
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
