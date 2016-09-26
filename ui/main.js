10f94c5a05a863f6f2421a73130a6f4ba46cfca0
vishnumohankv <vishnukizhakkumkara@gmail.com>
2016-09-26 20:56:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change the text of main text div
-var element = document.getElementById('main-text');
+var element = document.getElementById('text-elt');
 element.InnerHTML='vishnu';
