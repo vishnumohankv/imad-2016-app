b911b9f5763446f7daffc7df0df74f73389e1e0c
vishnumohankv <vishnukizhakkumkara@gmail.com>
2016-10-04 21:35:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,21 @@ request.onreadystatechange = function (){
   request.send(null);
   
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('sbmt_btn');
+submit.onclick = function() {
+    //make rqst to the server and send the name
+     
+     //capture the names and render it as a list
+     var names = ['name1','name2','name3','name4','name5'];
+     var list = '';
+     for(var i=0; i<names.length; i++){
+         list += '<li>'+ names[i] +'</li>';
+         
+     }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
