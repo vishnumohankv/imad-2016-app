63455c7acbd48e8f147432541fc278c8d3e81af7
vishnumohankv <vishnukizhakkumkara@gmail.com>
2016-10-04 10:40:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.get('/article2', function (req, res) {
   res.send('article 1 will be sent');
 });
 
+var counter=0;
+app.get('/counter', function(req,res) {
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 }); 
